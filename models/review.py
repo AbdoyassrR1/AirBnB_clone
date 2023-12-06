e1dcbab9848c61c8cbf9b4fd19ed1c51c41c1f02
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-12-06 16:24:43 +0200

    review model

---
+++ models/review.py
@@ -0,0 +1,14 @@
+"""
+review model
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    review class
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
