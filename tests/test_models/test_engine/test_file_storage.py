9cfbf6345a50c6ae08dc48f0c2c4ed315322b110
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-12-10 15:55:40 +0200

    hmm

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,30 +1,9 @@
 #!/usr/bin/python3
 '''Module for file_storage tests.'''
 import unittest
-import models
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 import os
-from models.user import User
-from models.state import State
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-
-
-class TestFileStorage_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the FileStorage class."""
-
-    def test_FileStorage_instantiation_no_args(self):
-        self.assertEqual(type(FileStorage()), FileStorage)
-
-    def test_FileStorage_instantiation_with_arg(self):
-        with self.assertRaises(TypeError):
-            FileStorage(None)
-
-    def test_storage_initializes(self):
-        self.assertEqual(type(models.storage), FileStorage)
 
 
 class TestFileStorage(unittest.TestCase):
@@ -53,15 +32,6 @@ class TestFileStorage(unittest.TestCase):
         self.assertIsInstance(all_objects, dict)
         self.assertEqual(all_objects, self.file_storage._FileStorage__objects)
 
-    def test_all(self):
-        '''T2.0: test all'''
-        self.assertEqual(dict, type(models.storage.all()))
-
-    def test_all_with_arg(self):
-        """T2.1: Test All With Arguments"""
-        with self.assertRaises(TypeError):
-            models.storage.all(None)
-
     def test_new_method(self):
         '''T3: Ensure that the new method adds an instance to __objects'''
         model = BaseModel()
@@ -71,48 +41,6 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(
             self.file_storage._FileStorage__objects[key]
             .to_dict(), model.to_dict())
-
-    def test_new(self):
-        """T3.0: Test New"""
-        base_model = BaseModel()
-        user = User()
-        state = State()
-        place = Place()
-        city = City()
-        amenity = Amenity()
-        review = Review()
-        models.storage.new(base_model)
-        models.storage.new(user)
-        models.storage.new(state)
-        models.storage.new(place)
-        models.storage.new(city)
-        models.storage.new(amenity)
-        models.storage.new(review)
-        self.assertIn("BaseModel." + base_model.id,
-                      models.storage.all().keys())
-        self.assertIn(base_model, models.storage.all().values())
-        self.assertIn("User." + user.id, models.storage.all().keys())
-        self.assertIn(user, models.storage.all().values())
-        self.assertIn("State." + state.id, models.storage.all().keys())
-        self.assertIn(state, models.storage.all().values())
-        self.assertIn("Place." + place.id, models.storage.all().keys())
-        self.assertIn(place, models.storage.all().values())
-        self.assertIn("City." + city.id, models.storage.all().keys())
-        self.assertIn(city, models.storage.all().values())
-        self.assertIn("Amenity." + amenity.id, models.storage.all().keys())
-        self.assertIn(amenity, models.storage.all().values())
-        self.assertIn("Review." + review.id, models.storage.all().keys())
-        self.assertIn(review, models.storage.all().values())
-
-    def test_new_with_args(self):
-        """T3.1: Test New With Arguments"""
-        with self.assertRaises(TypeError):
-            models.storage.new(BaseModel(), 1)
-
-    def test_new_with_None(self):
-        """T3.2: Test New Without AnyThing"""
-        with self.assertRaises(AttributeError):
-            models.storage.new(None)
 
     def test_save_and_reload_methods(self):
         '''T4: Ensure that save and reload methods work together'''
@@ -124,39 +52,6 @@ class TestFileStorage(unittest.TestCase):
         reloaded_model = self.file_storage._FileStorage__objects.get(key)
         self.assertIsNotNone(reloaded_model)
         self.assertEqual(reloaded_model.to_dict(), model.to_dict())
-
-    def test_save(self):
-        """T4.0: Test Save"""
-        base_model = BaseModel()
-        user = User()
-        state = State()
-        place = Place()
-        city = City()
-        amenity = Amenity()
-        review = Review()
-        models.storage.new(base_model)
-        models.storage.new(user)
-        models.storage.new(state)
-        models.storage.new(place)
-        models.storage.new(city)
-        models.storage.new(amenity)
-        models.storage.new(review)
-        models.storage.save()
-        save_text = ""
-        with open("file.json", "r") as f:
-            save_text = f.read()
-            self.assertIn("BaseModel." + base_model.id, save_text)
-            self.assertIn("User." + user.id, save_text)
-            self.assertIn("State." + state.id, save_text)
-            self.assertIn("Place." + place.id, save_text)
-            self.assertIn("City." + city.id, save_text)
-            self.assertIn("Amenity." + amenity.id, save_text)
-            self.assertIn("Review." + review.id, save_text)
-
-    def test_save_with_arg(self):
-        """T4.1: Test Save With Arg"""
-        with self.assertRaises(TypeError):
-            models.storage.save(None)
 
     def test_reload_nonexistent_file(self):
         '''T5: Ensure that reloading from a
@@ -287,7 +182,6 @@ class TestFileStorage(unittest.TestCase):
         that does not have to_dict method does not raise an error'''
         class CustomModel:
             pass
-
         with self.assertRaises(AttributeError):
             custom_model = CustomModel()
             self.file_storage.new(custom_model)
@@ -297,21 +191,6 @@ class TestFileStorage(unittest.TestCase):
             with open(self.file_storage._FileStorage__file_path, 'r') as file:
                 content = file.read()
                 self.assertNotIn('CustomModel', content)
-
-    def test_new_with_None(self):
-        """T15:Test New Without AnyThing"""
-        with self.assertRaises(AttributeError):
-            models.storage.new(None)
-
-    def test_reload_with_arg(self):
-        """T16: Test Reload With Args"""
-        with self.assertRaises(TypeError):
-            models.storage.reload(None)
-
-    def test_save_with_arg(self):
-        """T17: Test Save With Arg"""
-        with self.assertRaises(TypeError):
-            models.storage.save(None)
 
 
 if __name__ == '__main__':
