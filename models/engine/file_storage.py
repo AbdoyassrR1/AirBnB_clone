8e96b946722c8a9b82a62e2594da2ffd220b5517
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-12-10 20:46:43 +0200

    Final no cap

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,30 +27,26 @@ class FileStorage:
         '''Class constructor'''
         pass
 
-    @classmethod
-    def all(cls):
+    def all(self):
         '''returns the dictionary __objects'''
-        return cls.__objects
+        return self.__objects
 
-    @classmethod
-    def new(cls, obj):
+    def new(self, obj):
         '''sets in __objects the obj with key <obj class name>.id'''
         # if obj is None:
         #     raise AttributeError
         # if obj.__class__.__name__ not in classLocations.keys():
         #     raise ValueError
         key = obj.__class__.__name__ + "." + obj.id
-        cls.__objects[key] = obj
+        self.__objects[key] = obj
 
-    @classmethod
-    def save(cls):
+    def save(self):
         '''serializes __objects to the JSON file (path: __file_path)'''
-        obj_dict = {key: obj.to_dict() for key, obj in cls.__objects.items()}
-        with open(cls.__file_path, 'w', encoding="utf-8") as f:
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
             json.dump(obj_dict, f)
 
-    @classmethod
-    def reload(cls):
+    def reload(self):
         '''
         deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists ;
@@ -58,7 +54,7 @@ class FileStorage:
         no exception should be raised)
         '''
         try:
-            with open(cls.__file_path, 'r', encoding="utf-8") as f:
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
                 data = json.load(f)
                 for key, value in data.items():
                     class_name, _ = key.split(".")
@@ -66,15 +62,14 @@ class FileStorage:
                                         fromlist=[class_name])
                     class_ = getattr(module, class_name)
                     instance = class_(**value)
-                    cls.__objects[key] = instance
+                    self.__objects[key] = instance
         except FileNotFoundError:
             return
         except json.decoder.JSONDecodeError:
             return
 
-    @classmethod
-    def delete(cls, key):
+    def delete(self, key):
         '''delete key in __objects and updates the JSON file'''
-        if key in cls.__objects.keys():
-            del cls.__objects[key]
-            cls.save()
+        if key in self.__objects.keys():
+            del self.__objects[key]
+            self.save()
