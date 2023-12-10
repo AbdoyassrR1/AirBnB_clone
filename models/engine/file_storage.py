5737523c595a49c6047958520455f6e06d2b222b
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-12-10 15:35:40 +0200

    attr err + tests 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,11 +31,11 @@ class FileStorage:
 
     def new(self, obj):
         '''sets in __objects the obj with key <obj class name>.id'''
-        try:
-            key = obj.__class__.__name__ + "." + obj.id
-            self.__objects[key] = obj
-        except AttributeError:
-            return
+        if obj is None:
+            raise AttributeError
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
+
 
     def save(self):
         '''serializes __objects to the JSON file (path: __file_path)'''
